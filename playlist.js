d1a0cc69bda63acafe47b84e1363ffeaba30dce5
yuta0801 <yuta030801@gmail.com>
2018-07-07 15:45:28 +0800

    再生リスト関連のメッセージをmessages.jsonに移動

--- playlist.js
+++ playlist.js
@@ -1,20 +1,22 @@
 const ypi = require(`youtube-playlist-info`);
+const messages = require(`./messages.json`);
+const format = require(`string-format`);
 
-module.exports = async (messages, text) => {
-  if (!text) return messages.quiz.please_playlistid;
-  if (text.length < 34) return `:x: 文字数が足りません(34文字以上であることが必須です)。`;
+module.exports = async (message, text) => {
+  if (!text) return message.quiz.please_playlistid;
+  if (text.length < 34) return messages.quiz.not_enough_count;
   if (text.length > 34) text = (text.match(/[&?]list=([^&]+)/i) || [])[1];
-  if (!text) return `再生リストが正しくありません`;
+  if (!text) return messages.quiz.invalid_playlistid;
   return await ypi(process.env.APIKEY, text)
     .catch((error) => {
       if (error === `Error: The request is not properly authorized to retrieve the specified playlist.`) {
-        return `:x: このプレイリストは非公開です。`;
+        return messages.quiz.error.unavailable;
       } else if (error === `Error: The playlist identified with the requests <code>playlistId</code> parameter cannot be found.`) {
-        return `:x: このプレイリストは存在しません。`;
+        return messages.quiz.error.notfound;
       } else if (error === `Error: Bad Request`) {
-        return `:x: Bad Request: YouTube Data APIキーが間違っている可能性があります。`;
+        return messages.quiz.error.badrequest;
       } else {
-        return `再生リスト読み込みエラー：\`${error}\``;
+        return format(messages.quiz.error.unknown_error, error);
       }
     });
 };
