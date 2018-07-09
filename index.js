da53806d2c4f92af9f398ec2836d6d91b5be0a94
yuta0801 <yuta20030801@icloud.com>
2018-07-09 14:36:17 +0800

    ESLintのルールを更新

--- index.js
+++ index.js
@@ -13,102 +13,102 @@
 //
 
 /* Clear cache */
-delete require.cache[require.resolve(`./messages.json`)];
+delete require.cache[require.resolve(`./messages.json`)]
 
-const {"parsed": env} = require(`dotenv-safe`).config();
-const discord = require(`discord.js`);
-const client = new discord.Client();
-const ytdl = require(`ytdl-core`);
-const playlist = require(`./playlist`);
+const {"parsed": env} = require(`dotenv-safe`).config()
+const discord = require(`discord.js`)
+const client = new discord.Client()
+const ytdl = require(`ytdl-core`)
+const playlist = require(`./playlist`)
 const {
   songReplace,
   songReplace2,
   songReplace3
-} = require(`./song_replace`);
-const messages = require(`./messages.json`);
-const levenshtein = require(`levenshtein`);
-const commands = {};
+} = require(`./song_replace`)
+const messages = require(`./messages.json`)
+const levenshtein = require(`levenshtein`)
+const commands = {}
 
-let status = false;
-let correct = false;
-let songinfo = ``;
-let connection = ``;
-let dispatcher;
-let timeout;
-let songs;
+let status = false
+let correct = false
+let songinfo = ``
+let connection = ``
+let dispatcher
+let timeout
+let songs
 
 client.on(`ready`, () => {
-  console.log(messages.console.login_complete, client.user.tag);
-});
+  console.log(messages.console.login_complete, client.user.tag)
+})
 
-client.on(`message`, async (msg) => {
-  if (!msg.guild) return;
-  if (msg.author.bot || msg.system) return;
+client.on(`message`, async msg => {
+  if (!msg.guild) return
+  if (msg.author.bot || msg.system) return
   if (msg.content.startsWith(env.PREFIX)) {
-    console.log(`${msg.author.tag}がコマンドを送信しました: ${msg.content}`);
-    const split = msg.content.replace(env.PREFIX, ``).split(` `);
-    const command = split[0];
-    if (commands[command]) commands[command].run(msg, split);
+    console.log(`${msg.author.tag}がコマンドを送信しました: ${msg.content}`)
+    const split = msg.content.replace(env.PREFIX, ``).split(` `)
+    const command = split[0]
+    if (commands[command]) commands[command].run(msg, split)
     else {
-      const cmds = Object.keys(commands);
-      const commandList = cmds.map((cmd) => ({
+      const cmds = Object.keys(commands)
+      const commandList = cmds.map(cmd => ({
         "command": cmd,
         "levenshtein": levenshtein(split[0], cmd).distance
-      }));
-      const similarCmds = commandList.filter((e) => e.levenshtein <= 2)
+      }))
+      const similarCmds = commandList.filter(e => e.levenshtein <= 2)
         .sort((a, b) => a.no - b.no)
-        .map((e) => `・\`${env.PREFIX}${e.command}\``)
-        .join(`\n`);
-      if (!similarCmds) return;
-      msg.channel.send(messages.no_command);
-      msg.channel.send(messages.didyoumean(similarCmds));
+        .map(e => `・\`${env.PREFIX}${e.command}\``)
+        .join(`\n`)
+      if (!similarCmds) return
+      msg.channel.send(messages.no_command)
+      msg.channel.send(messages.didyoumean(similarCmds))
     }
   } else if (status) {
-    const answera = songReplace(songinfo[1]); // pickup answer
-    const answerb = songReplace2(songinfo[1]); // pickup another answer
-    const answerc = songReplace3(songinfo[1]); // pickup another another answer
+    const answera = songReplace(songinfo[1]) // pickup answer
+    const answerb = songReplace2(songinfo[1]) // pickup another answer
+    const answerc = songReplace3(songinfo[1]) // pickup another another answer
     if (msg.content.includes(answera) || msg.content.includes(answerb) || msg.content.includes(answerc)) {
-      correct = true;
-      msg.channel.send(messages.quiz.correct(songinfo[1], songinfo[0]));
-      dispatcher.end();
+      correct = true
+      msg.channel.send(messages.quiz.correct(songinfo[1], songinfo[0]))
+      dispatcher.end()
     }
   }
-});
+})
 commands.ping = {
   "description": `ボットのPingを確認`,
   "usage": [[`ping`, `ボットのPingを確認`]],
   run(msg) {
-    msg.channel.send(messages.pong(Math.floor(client.ping)));
+    msg.channel.send(messages.pong(Math.floor(client.ping)))
   }
-};
+}
 
 commands.help = {
   "description": `ヘルプを表示`,
   "usage": [[`help [<command>]`, `ヘルプを表示`]],
   run(msg, split) {
     if (split[1]) {
-      const cmd = commands[split[1]];
+      const cmd = commands[split[1]]
       if (!cmd || !cmd.description && !cmd.usage)
-        return msg.channel.send(messages.no_command);
+        return msg.channel.send(messages.no_command)
       const embed = new discord.RichEmbed()
         .setTitle(split[1])
-        .setTimestamp();
-      if (cmd.description) embed.setDescription(cmd.description);
-      if (cmd.usage) cmd.usage.forEach((usage) => embed.addField(...usage));
-      msg.channel.send(embed);
+        .setTimestamp()
+      if (cmd.description) embed.setDescription(cmd.description)
+      if (cmd.usage) cmd.usage.forEach(usage => embed.addField(...usage))
+      msg.channel.send(embed)
     } else {
       const embed = new discord.RichEmbed()
         .setTitle(messages.help.commands)
-        .setTimestamp();
-      Object.keys(commands).forEach((cmd) => {
-        const command = commands[cmd];
+        .setTimestamp()
+      Object.keys(commands).forEach(cmd => {
+        const command = commands[cmd]
         if (command.description)
-          embed.addField(cmd, command.description);
-      });
-      msg.channel.send(embed);
+          embed.addField(cmd, command.description)
+      })
+      msg.channel.send(embed)
     }
   }
-};
+}
 
 commands.connect = {
   "description": `ボイスチャンネルに接続`,
@@ -117,38 +117,38 @@ commands.connect = {
     if (msg.member.voiceChannel) {
       msg.member.voiceChannel.join().then(() =>
         msg.channel.send(messages.join_vc.success(msg.member.voiceChannel.name))
-      ).catch((error) => {
+      ).catch(error => {
         if (msg.member.voiceChannel.full) {
-          msg.channel.send(messages.join_vc.full(msg.member.voiceChannel.name));
+          msg.channel.send(messages.join_vc.full(msg.member.voiceChannel.name))
         } else if (!msg.member.voiceChannel.joinable) {
-          msg.channel.send(messages.join_vc.no_permission(msg.member.voiceChannel.name));
+          msg.channel.send(messages.join_vc.no_permission(msg.member.voiceChannel.name))
         } else {
-          msg.channel.send(messages.join_vc.unknown_error(msg.member.voiceChannel.name));
-          console.error(messages.console.join_vc_error, error);
+          msg.channel.send(messages.join_vc.unknown_error(msg.member.voiceChannel.name))
+          console.error(messages.console.join_vc_error, error)
         }
-      });
+      })
     } else {
-      msg.channel.send(messages.join_vc.tryagain);
+      msg.channel.send(messages.join_vc.tryagain)
     }
   }
-};
+}
 
 commands.disconnect = {
   "description": `ボイスチャンネルから切断`,
   "usage": [[`disconnect`, `ボイスチャンネルから切断`]],
   run(msg) {
-    client.clearTimeout(timeout);
+    client.clearTimeout(timeout)
     if (status) {
-      status = false;
-      correct = false;
-      dispatcher.end();
-      connection.disconnect();
+      status = false
+      correct = false
+      dispatcher.end()
+      connection.disconnect()
     }
-    if (!msg.guild.me.voiceChannel) return msg.channel.send(messages.exit_vc_notjoined);
-    msg.guild.me.voiceChannel.leave();
-    msg.channel.send(messages.exit_vc(msg.guild.me.voiceChannel.name));
+    if (!msg.guild.me.voiceChannel) return msg.channel.send(messages.exit_vc_notjoined)
+    msg.guild.me.voiceChannel.leave()
+    msg.channel.send(messages.exit_vc(msg.guild.me.voiceChannel.name))
   }
-};
+}
 
 commands.quiz = {
   "description": `イントロクイズを開始、終了`,
@@ -158,80 +158,80 @@ commands.quiz = {
   ],
   async run(msg, split) {
     if (split[1] === `start`) {
-      if (status) return;
+      if (status) return
       if (msg.member.voiceChannel) {
-        msg.channel.send(messages.quiz.loading);
-        const list = await playlist(split[2]);
-        if (!Array.isArray(list)) return msg.channel.send(list);
-        songs = list.map((video) => [video.resourceId.videoId, video.title]);
-        msg.member.voiceChannel.join().then((con) => {
-          connection = con;
-          status = true;
-          nextquiz(msg);
-        }).catch((error) => {
+        msg.channel.send(messages.quiz.loading)
+        const list = await playlist(split[2])
+        if (!Array.isArray(list)) return msg.channel.send(list)
+        songs = list.map(video => [video.resourceId.videoId, video.title])
+        msg.member.voiceChannel.join().then(con => {
+          connection = con
+          status = true
+          nextquiz(msg)
+        }).catch(error => {
           if (msg.member.voiceChannel.full) {
-            msg.channel.send(messages.join_vc.full(msg.member.voiceChannel.name));
+            msg.channel.send(messages.join_vc.full(msg.member.voiceChannel.name))
           } else if (!msg.member.voiceChannel.joinable) {
-            msg.channel.send(messages.join_vc.no_permission(msg.member.voiceChannel.name));
+            msg.channel.send(messages.join_vc.no_permission(msg.member.voiceChannel.name))
           } else {
-            msg.channel.send(messages.join_vc.unknown_error(msg.member.voiceChannel.name));
-            console.error(messages.console.join_vc_error, error);
+            msg.channel.send(messages.join_vc.unknown_error(msg.member.voiceChannel.name))
+            console.error(messages.console.join_vc_error, error)
           }
-        });
+        })
       } else {
-        msg.channel.send(messages.join_vc.tryagain);
+        msg.channel.send(messages.join_vc.tryagain)
       }
     } else if (split[1] === `end` || split[1] === `stop`) {
       if (status) {
-        client.clearTimeout(timeout);
-        status = false;
-        correct = false;
-        dispatcher.end();
-        connection.disconnect();
-        msg.channel.send(messages.quiz.stop);
+        client.clearTimeout(timeout)
+        status = false
+        correct = false
+        dispatcher.end()
+        connection.disconnect()
+        msg.channel.send(messages.quiz.stop)
       } else {
-        msg.channel.send(messages.quiz.not_started);
+        msg.channel.send(messages.quiz.not_started)
       }
     } else {
-      msg.channel.send(messages.wrong_args);
+      msg.channel.send(messages.wrong_args)
     }
   }
-};
+}
 
 function nextquiz(msg, number = 0) {
-  msg.channel.send(messages.quiz.nextquiz(++number));
-  correct = false;
+  msg.channel.send(messages.quiz.nextquiz(++number))
+  correct = false
   timeout = client.setTimeout(() => {
-    msg.channel.send(messages.quiz.start);
-    songinfo = songs[Math.floor(Math.random() * songs.length)];
-    console.log(songinfo);
-    const stream = ytdl(songinfo[0], {"filter": `audioonly`});
-    dispatcher = connection.playStream(stream);
+    msg.channel.send(messages.quiz.start)
+    songinfo = songs[Math.floor(Math.random() * songs.length)]
+    console.log(songinfo)
+    const stream = ytdl(songinfo[0], {"filter": `audioonly`})
+    dispatcher = connection.playStream(stream)
     dispatcher.on(`end`, () => {
       if (!correct)
-        msg.channel.send(messages.quiz.uncorrect(songinfo[1], songinfo[0]));
-      if (status) nextquiz(msg, number);
-    });
-  }, 5000);
+        msg.channel.send(messages.quiz.uncorrect(songinfo[1], songinfo[0]))
+      if (status) nextquiz(msg, number)
+    })
+  }, 5000)
 }
 
 commands.test = {
   run(msg) {
-    msg.channel.send(`Extracted name: \`` + songReplace(msg.content.replace(env.PREFIX + `test `, ``)) + `\``);
+    msg.channel.send(`Extracted name: \`` + songReplace(msg.content.replace(env.PREFIX + `test `, ``)) + `\``)
   }
-};
+}
 
 commands.test2 = {
   run(msg) {
-    msg.channel.send(`Extracted name: \`` + songReplace2(msg.content.replace(env.PREFIX + `test2 `, ``)) + `\``);
+    msg.channel.send(`Extracted name: \`` + songReplace2(msg.content.replace(env.PREFIX + `test2 `, ``)) + `\``)
   }
-};
+}
 
 commands.test3 = {
   run(msg) {
-    msg.channel.send(`Extracted name: \`` + songReplace3(msg.content.replace(env.PREFIX + `test3 `, ``)) + `\``);
+    msg.channel.send(`Extracted name: \`` + songReplace3(msg.content.replace(env.PREFIX + `test3 `, ``)) + `\``)
   }
-};
+}
 
 commands.testmulti = {
   run(msg) {
@@ -240,20 +240,20 @@ commands.testmulti = {
       .addField(`1つ目の答え`, `\`` + songReplace(msg.content.replace(env.PREFIX + `testmulti `, ``)) + `\``)
       .addField(`2つ目の答え`, `\`` + songReplace2(msg.content.replace(env.PREFIX + `testmulti `, ``)) + `\``)
       .addField(`3つ目の答え`, `\`` + songReplace3(msg.content.replace(env.PREFIX + `testmulti `, ``)) + `\``)
-      .setFooter(`元テキスト: \`` + msg.content + `\` / コマンド抜き: \`` + msg.content.replace(env.PREFIX + `testmulti `, ``) + `\``);
-    msg.channel.send(embed);
+      .setFooter(`元テキスト: \`` + msg.content + `\` / コマンド抜き: \`` + msg.content.replace(env.PREFIX + `testmulti `, ``) + `\``)
+    msg.channel.send(embed)
   }
-};
+}
 
 process.on(`SIGINT`, () => {
   setTimeout(() => {
-    console.error(`強制終了中...`);
-    process.exit();
-  }, 5000);
-  console.error(`SIGINTを検知しました。`);
-  client.destroy();
-});
+    console.error(`強制終了中...`)
+    process.exit()
+  }, 5000)
+  console.error(`SIGINTを検知しました。`)
+  client.destroy()
+})
 
-client.login(Buffer.from(Buffer.from(Buffer.from(env.TOKEN, `base64`).toString(`ascii`), `base64`).toString(`ascii`), `base64`).toString(`ascii`));
+client.login(Buffer.from(Buffer.from(Buffer.from(env.TOKEN, `base64`).toString(`ascii`), `base64`).toString(`ascii`), `base64`).toString(`ascii`))
 
-process.on(`unhandledRejection`, console.error);
+process.on(`unhandledRejection`, console.error)
--- playlist.js
+++ playlist.js
@@ -1,21 +1,21 @@
-const ypi = require(`youtube-playlist-info`);
-const messages = require(`./messages.json`);
+const ypi = require(`youtube-playlist-info`)
+const messages = require(`./messages.json`)
 
-module.exports = async (text) => {
-  if (!text) return messages.quiz.please_playlist;
-  if (text.length < 34) text = null;
-  if (text.length > 34) text = (text.match(/[&?]list=([^&]+)/i) || [])[1];
-  if (!text) return messages.quiz.invalid_playlist;
+module.exports = async text => {
+  if (!text) return messages.quiz.please_playlist
+  if (text.length < 34) text = null
+  if (text.length > 34) text = (text.match(/[&?]list=([^&]+)/i) || [])[1]
+  if (!text) return messages.quiz.invalid_playlist
   return await ypi(process.env.APIKEY, text)
-    .catch((error) => {
+    .catch(error => {
       if (error === `Error: The request is not properly authorized to retrieve the specified playlist.`) {
-        return messages.quiz.error.unavailable;
+        return messages.quiz.error.unavailable
       } else if (error === `Error: The playlist identified with the requests <code>playlistId</code> parameter cannot be found.`) {
-        return messages.quiz.error.notfound;
+        return messages.quiz.error.notfound
       } else if (error === `Error: Bad Request`) {
-        return messages.quiz.error.badrequest;
+        return messages.quiz.error.badrequest
       } else {
-        return messages.quiz.error.unknown_error(error);
+        return messages.quiz.error.unknown_error(error)
       }
-    });
-};
+    })
+}
