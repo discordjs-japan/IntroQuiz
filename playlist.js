5b3d1001ab106c033a7407b1878c632083252cce
yuta0801 <yuta20030801@icloud.com>
2018-07-12 15:09:36 +0800

    messagesのキーを大文字に変更
    - camelcaseで警告されるため

--- playlist.js
+++ playlist.js
@@ -2,20 +2,20 @@ const ypi = require(`youtube-playlist-info`)
 const messages = require(`./messages.json`)
 
 module.exports = async text => {
-  if (!text) return messages.quiz.please_playlist
+  if (!text) return messages.QUIZ.PLEASE_PLAYLIST
   if (text.length < 34) text = null
   if (text.length > 34) text = (text.match(/[&?]list=([^&]+)/i) || [])[1]
-  if (!text) return messages.quiz.invalid_playlist
+  if (!text) return messages.QUIZ.INVALID_PLAYLIST
   return await ypi(process.env.APIKEY, text)
     .catch(error => {
       if (error === `Error: The request is not properly authorized to retrieve the specified playlist.`) {
-        return messages.quiz.error.unavailable
+        return messages.QUIZ.ERROR.UNAVAILABLE
       } else if (error === `Error: The playlist identified with the requests <code>playlistId</code> parameter cannot be found.`) {
-        return messages.quiz.error.notfound
+        return messages.QUIZ.ERROR.NOTFOUND
       } else if (error === `Error: Bad Request`) {
-        return messages.quiz.error.badrequest
+        return messages.QUIZ.ERROR.BADREQUEST
       } else {
-        return messages.quiz.error.unknown_error(error)
+        return messages.QUIZ.ERROR.UNKNOWN_ERROR(error)
       }
     })
 }
